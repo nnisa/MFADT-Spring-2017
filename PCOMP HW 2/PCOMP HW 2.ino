432ed5ce8bf7e6cdd1d76057d3afc98d5ee06fd0
nnisa <nisanoshin@gmail.com>
2017-02-26 17:32:11 -0500

    arduino tests

---
+++ PCOMP HW 2/PCOMP HW 2.ino
@@ -0,0 +1,31 @@
+
+const int trigPin = 8; 
+const int echoPin =13;
+
+long duration; 
+int distance;
+
+void setup() {
+  pinMode (trigPin, OUTPUT);
+  pinMode (echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2;
+  Serial.println("Distance:");
+  Serial.println(distance);
+}
+
+
+
+
+
